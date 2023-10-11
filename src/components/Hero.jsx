2a2840fe508acc9c6ede258470b6ae802c302e88
Łukasz Kałużny <lukasz@kaluzny.pro>
2023-10-11 08:58:51 +0000

    Nowy tekst

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,30 +9,29 @@ export function Hero() {
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
-            <span className="sr-only">Azure Day PL - </span>Azure Day PL
+            Azure Day PL 2024
           </h1>
           <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-blue-900">
             <p>
-              The next generation of web users are tech-savvy and suspicious.
-              They know how to use dev tools, they can detect a phishing scam
-              from a mile away, and they certainly aren’t accepting any checks
-              from Western Union.
+              Po krótkiej przerwie powracamy z kolejną, siódmą już edycją konferencji AzureDay Poland.
             </p>
             <p>
-              At DeceptiConf you’ll learn about the latest dark patterns being
-              developed to trick even the smartest visitors, and you’ll learn
-              how to deploy them without ever being detected.
+              Zapraszamy serdecznie wszystkich miłośników rozwiązań chmurowych już 15 lutego 2024 roku do Warszawy.
+              W trakcie sprzedaży biletów Early Birds nie ujawnimy więcej szczegółów, ale obiecujemy, że z uwagą przeczytaliśmy wszystkie komentarze i uwagi po przednich edycjach i wiemy, czego oczekujecie od tej konferencji - postaramy się Was nie zawieść.
+            </p>
+            <p>
+              Więcej informacji już wkrótce.
             </p>
           </div>
-          <Button href="#" className="mt-10 w-full sm:hidden">
-            Get your tickets
+          <Button href="https://azureday2024.konfeo.com/pl/groups" className="mt-10 w-full sm:hidden">
+            Kup bilet!
           </Button>
           <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
             {[
-              ['Speakers', '18'],
-              ['People Attending', '2,091'],
-              ['Venue', 'Staples Center'],
-              ['Location', 'Los Angeles'],
+              ['Prelegenci', '18'],
+              ['Uczestnicy', '300'],
+              ['Venue', 'Airport Hotel Okęcie'],
+              ['Miasto', 'Warszawa'],
             ].map(([name, value]) => (
               <div key={name}>
                 <dt className="font-mono text-sm text-blue-600">{name}</dt>
