2b51f89381b97d7e060fc798fde10e43b0104b28
Łukasz Kałużny <lukasz@kaluzny.pro>
2023-11-05 09:08:34 +0000

    Orgs

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,15 +1,18 @@
 import { CFP } from '@/components/Cfp'
 import { Hero } from '@/components/Hero'
 import { Newsletter } from '@/components/Newsletter'
+import Price from '@/components/Price'
 import { Schedule } from '@/components/Schedule'
 import { Speakers } from '@/components/Speakers'
 import { Sponsors } from '@/components/Sponsors'
+import { Organizers } from '@/components/Organizers'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <CFP />
+      <Organizers />
       <Sponsors />
     </>
   )
